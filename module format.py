de6e02a889046a5a17ea8971d2691fa9b0808d79
Tanish Sarmah <70163670+tanishshadow@users.noreply.github.com>
2021-06-24 17:20:04 +0530

    Create module format.py

---
+++ module format.py
@@ -0,0 +1,16 @@
+#CalculateArea
+"""Calculates the area of a square , rectangle , circle"""
+
+# Functions 
+def rectangle(length,breadth):
+    "Returns the area of a rectangle"
+    return length*breadth
+def circle(radius):
+    "Returns the area of a circle"
+    return pi*radius*radius
+def square(side):
+    "Returns the area of a square"
+    return side*side
+
+# Constants
+pi=3.14
