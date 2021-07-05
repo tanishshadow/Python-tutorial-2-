8118f6fefee599c199f57644e64e93816e111be9
Tanish Sarmah <70163670+tanishshadow@users.noreply.github.com>
2021-07-05 16:23:46 +0530

    Update map function .py

--- map function .py
+++ map function .py
@@ -1,3 +1,24 @@
+# Demonstrating the use of map function in python
+'''
+Syntax :
+
+map(fun, iter)
+Parameters :
+
+fun : It is a function to which map passes each element of given iterable.
+iter : It is a iterable which is to be mapped.
+
+NOTE : You can pass one or more iterable to the map() function.
+
+
+
+Returns :
+
+Returns a list of the results after applying the given function  
+to each item of a given iterable (list, tuple etc.) 
+'''
+
+
 def prod(n):
     return n*n
 
