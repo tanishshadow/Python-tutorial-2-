6d4e312fed9c0db99d5f52de8c8f386010635eff
Tanish Sarmah <70163670+tanishshadow@users.noreply.github.com>
2021-07-06 15:37:22 +0530

    Update default args of function.py

--- default args of function.py
+++ default args of function.py
@@ -5,3 +5,29 @@ def sum(int1=1,int2=2):
 print(sum(2,3))
 print(sum(2))
 print(sum())
+
+# Functions can also be called using keyword arguments of the form kwarg = value. For instance, the following function:
+
+
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.")
+    print("-- Lovely plumage, the", type)
+    print("-- It's", state, "!")
+
+
+# accepts one required argument(voltage) and three optional arguments(state, action, and type). This function can be called in any of the following ways:
+
+parrot(1000)                                          # 1 positional argument
+parrot(voltage=1000)                                  # 1 keyword argument
+parrot(voltage=1000000, action='VOOOOOM')             # 2 keyword arguments
+parrot(action='VOOOOOM', voltage=1000000)             # 2 keyword arguments
+parrot('a million', 'bereft of life', 'jump')         # 3 positional arguments
+parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword
+
+# but all the following calls would be invalid:
+
+parrot()                     # required argument missing
+parrot(voltage=5.0, 'dead')  # non-keyword argument after a keyword argument
+parrot(110, voltage=220)     # duplicate value for the same argument
+parrot(actor='John Cleese')  # unknown keyword argument
